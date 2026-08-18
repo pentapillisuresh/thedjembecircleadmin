776db868c57b62fdd6d053d376eaa6b670daaa2a
ravi kumar <ark.kumar03@gmail.com>
2026-08-19 00:20:02 +0530

    rrr

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
 server: {
   proxy: {
     '/api': {
-      target: 'http://localhost:3001',
+      target: 'https://service.thedjembecircle.com',
       changeOrigin: true,
     },
 
     '/uploads': {
-      target: 'http://localhost:3001',
+      target: 'https://service.thedjembecircle.com',
       changeOrigin: true,
     },
   },
